7dd4c2e294a388ad3c851f81150abced3630e022
andrejmoltok <csokan.andras87@protonmail.ch>
2024-10-27 19:29:52 +0100

    AQ-1 Setup: NextJS base environment and components

---
+++ src/components/map.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export default function Map() {
+  return (
+    <React.Fragment>
+      <svg width="803" height="577" xmlns="http://www.w3.org/2000/svg">
+        <image href="/veronia.jpg" width="803" height="577" />
+      </svg>
+    </React.Fragment>
+  );
+}
